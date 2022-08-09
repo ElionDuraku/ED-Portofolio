757751c965f9d0d5fe8ac4bd06c4548690cf74f6
Your Name <you@example.com>
2022-08-09 16:42:39 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:[" " ,"Social Media Assistant","Web Designer","Web Developer","Graphic Designer","Social Media Manager"],
+    typeSpeed:100,
+    BackSpeed:90,
+    loop:true
+})
